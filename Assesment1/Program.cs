2be702ecd8186da001654255f2958925e0e2767a
Creel Okafor <okaforcreel@gmail.com>
2023-04-03 16:52:16 +0100

    Add project files.

---
+++ Assesment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assesment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            FizzBuzz(100);
+        }
+
+
+        static void FizzBuzz(int row )
+        {
+            for ( int i = 1 ; i < row; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz" + " ");
+                }
+                   
+                else if ( i % 5 == 0)
+                {
+                    Console.Write("Buzz" + " ");
+
+            }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz" + " ");
+                }
+                else
+                 Console.Write(i + " "); 
+            }
+        }
+    }
+}
